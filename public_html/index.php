93e724ac1e050302e1a32ed5bc5087fae2c668ef
tim8n <timon.weiss@student.jade-hs.de>
2023-09-30 13:38:05 +0200

    ok letztes hier

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,19 @@
 </head>
 
 <body>
+	<h1>Das erste Programm</h1>
+	<?php
+		// Programmcode für die Berechnung der Zeit
+		/*
+		Mehrzeiliger Programm-
+		code
+		*/
+		$hoehe=10000;
+		echo(sqrt((2*$hoehe)/(9.81)));
 	
+	
+	
+	?>
 	<link href="F Kolb/index.php">
 	<link href="S Bosse/index.php">
 	<link href="T Weiß/index.php">
