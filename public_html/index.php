9a7edf5fe71ea8a024266d54409bdbd9267ad784
KolbFlorian <florian.kolb@student.jade-hs.de>
2023-09-30 15:26:28 +0200

    Bei mir funktioniert jetzt alles

--- public_html/index.php
+++ public_html/index.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Nerviges Dokument </title>
+<title>Unbenanntes Dokument</title>
 </head>
-
 <body>
+
 	<h1>Das erste Programm</h1>
 	<?php
 		// Programmcode für die Berechnung der Zeit
@@ -28,5 +28,6 @@
 		<a href="rechteck/rechteck.php" >Rechteck</a>
 		<a href="bmi/bmi.php" >BMI</a>
 		<a href="bandbreite/bandbreite.php" >Bandbreite</a>
+	
 </body>
 </html>
