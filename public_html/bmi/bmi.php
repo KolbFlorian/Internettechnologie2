dfecaf509c9441ca2b29936fd9ff2d19c9e17811
tim8n <timon.weiss@student.jade-hs.de>
2023-09-30 14:33:50 +0200

    erste Schritte

--- public_html/bmi/bmi.php
+++ public_html/bmi/bmi.php
@@ -4,7 +4,21 @@
 <meta charset="utf-8">
 <title>BMI-Aufgabe</title>
 </head>
+	<body>
+<h1>BMI-Berechnung</h1>
+<? 
+	
+	// Programmcode für die Berechnung der Zeit
+		/*
+		Mehrzeiliger Programm-
+		code
+		*/
+		$koerpergewicht=65;
+		$koerpergroesse=175;
+		echo(sqrt(($koerpergewicht)/($koerpergroesse)));
+	
 
-<body>
+	
+	?>
 </body>
 </html>
