2d0ef3a6d00a2e8da5b0f69f8514f44196912613
KolbFlorian <florian.kolb@student.jade-hs.de>
2023-10-03 14:54:04 +0200

    Richtige Verlinkungen
    Bei mir läuft die Seite jetzt einwandfrei mit den neuen Serverdaten der HS.

--- public_html/index.php
+++ public_html/index.php
@@ -7,27 +7,16 @@
 <body>
 
 	<h1>Das erste Programm</h1>
-	<?php
-		// Programmcode für die Berechnung der Zeit
-		/*
-		Mehrzeiliger Programm-
-		code
-		*/
-		$hoehe=10000;
-		echo(sqrt((2*$hoehe)/(9.81)));
 	
-	
-	
-	?>
-	<a href="S Bosse/index.php" >Steffen</a>
-	<a href="F Kolb/index.php" >Florian</a>
-	<a href="T Weiß/index.php" >Timon</a>
-	<a href="fallzeit/fallzeit.php" >Fallzeit</a>
-		<a href="tank/tank.php" >Tank</a>
-		<a href="gleichung/gleichung.php" >Gleichung</a>
-		<a href="rechteck/rechteck.php" >Rechteck</a>
-		<a href="bmi/bmi.php" >BMI</a>
-		<a href="bandbreite/bandbreite.php" >Bandbreite</a>
+	<a href="/S Bosse/index.php" >Steffen</a>
+	<a href="/F Kolb/index.php" >Florian</a>
+	<a href="/T Weiß/index.php" >Timon</a>
+		<a href="/fallzeit.php" >Fallzeit</a>
+		<a href="/tank/tank.php" >Tank</a>
+		<a href="/gleichung/gleichung.php" >Gleichung</a>
+		<a href="/rechteck/rechteck.php" >Rechteck</a>
+		<a href="/bmi/bmi.php" >BMI</a>
+		<a href="/bandbreite/bandbreite.php" >Bandbreite</a>
 	
 </body>
 </html>
--- public_html/rechteck/rechteck.php
+++ public_html/rechteck/rechteck.php
@@ -4,7 +4,18 @@
 <meta charset="utf-8">
 <title>Rechteck-Aufgabe</title>
 </head>
+	<?php
+	// Programmcode für die Berechnung der Fallzeit
+		/*
+		Mehrzeiliger Programm-
+		code
+		*/
+		$seiteA=10000;
+		$seiteB=10000;
+		echo($seiteA*$seiteB);
 
+	
+	?>
 <body>
 </body>
 </html>
