18e643238c1bb44eb5e7b371a848c9e01bc9f58c
KolbFlorian <florian.kolb@student.jade-hs.de>
2023-10-11 19:16:25 +0200

    Zu schwer der Müll
    Ich scheiß auf die Aufgaben. Alles was ich probiere funktioniert nicht

--- public_html/vorlesungswoche3/fallzeitergebnis.php
+++ public_html/vorlesungswoche3/fallzeitergebnis.php
@@ -21,26 +21,71 @@
 	// Zeichenkette in Zahlen umwandeln
 	$hoehe=intval($hoehe);
 	$himmelskoerper=intval($himmelskoerper);
-	$erde=$himmelskoerper+"erde";
-	
-	
-	if($hoehe>15)
+	/*
+	if($himmelskoerper="erde")
 		{
-		// Rechnung durchführen
-		$fallzeitergebnis=sqrt((2*$hoehe)/($himmelskoerper));
-	
-		// Ergebnis ausgeben
-		echo  " Die Wurzel aus (2 * $hoehe / $himmelskoerper = ".$fallzeitergebnis;
-		}
-	else if ($himmelskoerper="erde")
-	{
-		$fallzeitergebnis=sqrt((2*$hoehe)/(9.81));
+		$himmelskoerper=9.81;
 		
+	if($himmelskoerper="mond")
+		{
+		$himmelskoerper=1.62;
+		
+	if($himmelskoerper="mars")
+		{
+		$himmelskoerper=3.69;
+		
+	if($himmelskoerper="jupiter")
+		{
+		$himmelskoerper=24.79;
+		}
+		}
+		}
+		}
+	
+	switch ($himmelskoerper)
+	{
+		case "erde": $himmelskoerper=9.81;break;
+		case "mond": $himmelskoerper=1.62;break;
+		case "mars": $himmelskoerper=3.69;break;
+		case "jupiter": $himmelskoerper=24.79;break;	
+		default : "Wäre cool wenn Du einer von vier Planeten nimmst";break;
 	}
+	
+	*/
+	
+	$himmelskoerper=array("erde","mond","mars","jupiter");
+		
+		if($himmelskoerper[0])
+		{
+			$himmelskoerper=9.81;
+		}
+	
+	
+		if($himmelskoerper[1])
+		{
+			$himmelskoerper=1.62;
+		}
+	
+		if($himmelskoerper[2])
+		{
+			$himmelskoerper=3.69;
+		}
+	
+		if($himmelskoerper[3])
+		{
+			$himmelskoerper=24.79;
+		}
+	
+		
+		if($hoehe>15)
+		{
+		$fallzeitergebnis=sqrt((2*$hoehe)/($himmelskoerper));
+		echo  " sqrt( 2* $hoehe m / $himmelskoerper m/s) =  ".$fallzeitergebnis. " s";
+		}
+
 			else
 				{
 				echo "Die Höhe muss größer als 15 sein";
-			
 				}
 		
 				
