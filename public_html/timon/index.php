6d2d6d6d2490b9d5f7c17334f42bb18ee0185cd6
tim8n <timon.weiss@gmx.de>
2023-10-16 13:37:12 +0200

    hallo

--- public_html/timon/index.php
+++ public_html/timon/index.php
@@ -1,16 +1,39 @@
 <!doctype html>
 <html>
-<head>
-<<<<<<< HEAD
-<meta charset="utf-8"> 
-<title>IT2 </title>
-	
+<head>	
 <meta charset="utf-8">
 <title>Florian = Brillenschlange</title>
+	<link rel="stylesheet" type="text/css" href="../stylesheet/style.css">
 </head>
 <body>
 	
-	
+		<table border="1">
+	<?php
+	                                          //zeilen Start ; Ende ; Schrittweite
+		for($zeilenzaehler = 1;               //startwert zeilenzahler = 1
+		   $zeilenzaehler <= 10;              // wenn <=8 geht weiter, wenn nicht wird abgebrochen
+		   $zeilenzaehler ++ )                // -> zeilenzähler + 1 (++) also Dann zeilenzähler = 2 , (--) = - 1 
+		
+		{
+			echo "<tr>";
+			
+			for($spaltenzaehler = 1;
+			   $spaltenzaehler <= 10;
+			   $spaltenzaehler ++ )
+				
+			{	
+					$ergebnis = $zeilenzaehler * $spaltenzaehler;
+					
+					echo "<td>";
+					echo $ergebnis;
+					echo "</td>";
+			}
+				echo "</tr>"; 
+					
+		}
+		
+	?>
+	</table>
 	
 	
 	
