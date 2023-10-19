2e0a4d33e939468bbc2ed16b663d33c8e3d536da
tim8n <timon.weiss@gmx.de>
2023-10-19 09:44:21 +0200

    jo

---
+++ public_html/Website/pages/include/head_inc.php
@@ -0,0 +1,12 @@
+<<<<<<< Updated upstream
+=======
+<?php
+echo("123");
+	include('pages/team.php');
+	include('pages/menu_inc.php');
+	include('pages/head_inc.php');
+	include('pages/footer_inc.php');
+	include('pages/backLink_inc.php');
+
+?>
+>>>>>>> Stashed changes
