52b66b2476443e830ff99075c244dcbbfbbf02b0
tim8n <timon.weiss@gmx.de>
2023-10-26 09:07:52 +0200

    yessir

--- public_html/WebsiteNEU/pages/wuerfel.php
+++ public_html/WebsiteNEU/pages/wuerfel.php
@@ -2,9 +2,34 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Unbenanntes Dokument</title>
+<title>Würfel-Berechnung</title>
 </head>
 
 <body>
+	<h1>Mathematische Berechnung eines Würfels</h1>
+	<?php
+	//Daten aus Würfel übernehmen
+	$sl=$_POST["sl"];
+		
+	// Zeichenkette in zeilenzaehleren umwandeln
+	$sl=doubleval($sl);
+	
+	if($sl>0)
+	{
+		// Rechnung durchführen
+		$wvolumen=($sl*$sl*$sl);
+		$wmantel=((($sl*$sl)*4));
+		$woberfl=(6*($sl*$sl));
+	
+		// Ergebnis ausgeben
+		echo  " Das Volumen beträgt = ".$wvolumen; echo("<br>");
+		echo "Die Mantelfläche ...= ".$wmantel;echo("<br>");
+		echo "Die Oberfläche ...= ".$woberfl;
+    }
+	else echo("Die Seitenlänge muss größer als 0 sein")
+			
+	?>
+		<table border="1">
+	
 </body>
 </html>
